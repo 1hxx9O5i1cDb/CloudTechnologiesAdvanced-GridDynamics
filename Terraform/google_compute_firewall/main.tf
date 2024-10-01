deb2937d68880b6307ac69180cc41a8789cd43c9
1hxx9O5i1cDb <kprusak@griddynamics.com>
2024-10-01 18:45:50 +0200

    Almost done

--- Terraform/google_compute_firewall/main.tf
+++ Terraform/google_compute_firewall/main.tf
@@ -11,3 +11,29 @@ resource "google_compute_firewall" "allow_bastion_sql" {
     source_tags = [ "bastion-host" ]
     target_tags = [ "sql-server" ]
 }
+
+resource "google_compute_firewall" "allow_http" {
+    name = "allow-http"
+    description = "Allow HTTP"
+    network = var.network_uri
+
+    allow {
+        protocol = "tcp"
+        ports = [ "80" ]
+    }
+
+    source_ranges = [ "0.0.0.0/0" ]
+}
+
+resource "google_compute_firewall" "allow_https" {
+    name = "allow-https"
+    description = "Allow HTTPS"
+    network = var.network_uri
+
+    allow {
+        protocol = "tcp"
+        ports = [ "443" ]
+    }
+
+    source_ranges = [ "0.0.0.0/0" ]
+}
